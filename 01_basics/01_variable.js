93468cc31613b6fc730d548acb4e63a7b4cf2e79
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-01-18 14:01:06 +0000

    Conversion of data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId=1444553
+let accountEmail="vvkkkk@gmail.com"
+var accountPassward="12345"
+/* preferd to not use var 
+because of functional and block scopr*/
+accountCity="Jaipur"
+
+// accountId=2 // not allowed
+console.log(accountId);
+
+accountEmail="siuoieueiuui@gmial.com"
+accountPassward="23456"
+accountCity="nothing"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassward,accountCity]);
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,23 @@
+let score =33 
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof(score));
+//if i convert "33" => 33
+//"33asdf" =>NaN
+//true =>1; False=>0
+
+let isLoggedIn ="";
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=.true; 0=>false;
+//""=>false ; " "=>true
+//"vivek"=>true
+
+let s=33
+let stringNumber=String(x)
+console.log(stringNumber);
+console.log(typeof stringNumber);
