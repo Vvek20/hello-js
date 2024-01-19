5d1bb868937dee970e87e7d8e36bad8f324315cd
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-01-19 15:33:05 +0000

    add

---
+++ 01_basics/02_basics/03_object.js
@@ -0,0 +1,15 @@
+//object literals
+const jsUser={
+    name:'vivek',
+    age:18,
+    location:"Azamgarh",
+    email:"hdjh@gamil.com",
+    isLoggedIn:false,
+    lastLoginDays:["monday","Saturady"]
+}
+console.log(jsUser.email);
+//or u can also excess
+console.log(jsUser["email"])
+
+
+
