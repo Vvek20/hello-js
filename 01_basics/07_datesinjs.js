32aacef1bd2cab79af2991b0faf355559adf7bf5
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-01-19 11:23:14 +0000

    add 07

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,19 @@
+ //Dates
+  let myDate=new Date()
+  //console.log(myDate.toString());
+  //console.log(myDate.toLocaleDateString());
+  //console.log(myDate.toISOString());
+  //console.log(myDate.toJSON());
+  //console.log(typeof myDate);
+  let myCreateDate=new Date(2024,0,23)
+  //console.log(myCreateDate.toDateString());
+  let myTimeStamp=Date.now()
+  //console.log(myTimeStamp);
+  //console.log(myCreateDate.getTime());
+  //console.log(Date.now()/1000);
+  let newDate= new Date()
+  //console.log(newDate);
+  //console.log(newDate.getDate());
+  //console.log(newDate.getDay());
+  console.log(newDate.getMonth()+1);
+  newDat
