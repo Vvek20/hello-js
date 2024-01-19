df65a8f4515708172f614b6a59b15fd68abcef18
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-01-19 13:10:01 +0000

    add

---
+++ 01_basics/02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+// array 
+const myArr=[0,1,2,3,4,5]
+const myHeros=['shaktinam','naagarj']
+const myArr2=new Array(2,3,4,5)
+console.log(myArr[1]);
+
+// Array mathods
+
+//myArr.push(6)
+//myArr.push(7)
+console.log(myArr);
+myArr.pop()
+console.log(myArr);
