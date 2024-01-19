ef3007abcbeff96a70d185cb3425a39d384e7b87
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-01-19 15:20:17 +0000

    add

--- 01_basics/02_basics/02_array.js
+++ 01_basics/02_basics/02_array.js
@@ -1,4 +1,10 @@
 const marval_heros=["ironman","thor","spiderman"]
 const dc_heros=["Superman","flash","aquaman"]
 marval_heros.push(dc_heros)
-console.log(marval_heros);
+//console.log(marval_heros);
+//console.log(marval_heros[3][1]);
+
+const allHeros=marval_heros.concat(dc_heros)
+console.log(allHeros);
+const all_new_heros=[...marval_heros,...dc_heros]
+console.log(all_new_heros);
