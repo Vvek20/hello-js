6d2918f18210ed7f692f2418cabe59fc9f10ef82
Vvek20 <103495895+Vvek20@users.noreply.github.com>
2024-01-23 08:07:11 +0000

    add

--- 01_basics/02_basics/03_object.js
+++ 01_basics/02_basics/03_object.js
@@ -1,15 +1,38 @@
-//object literals
-const jsUser={
-    name:'vivek',
-    age:18,
-    location:"Azamgarh",
-    email:"hdjh@gamil.com",
-    isLoggedIn:false,
-    lastLoginDays:["monday","Saturady"]
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Vivek",
+    "full name": "Vivek Kuamar",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Azamgarh",
+    email: "Vievk@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
 }
-console.log(jsUser.email);
-//or u can also excess
-console.log(jsUser["email"])
 
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
 
+JsUser.email = "vivek@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "vivek@microsoft.com"
+// console.log(JsUser);
 
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
